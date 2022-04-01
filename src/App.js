ca3d0bef56a8823e70e89bf69a4ba2174a759eb4
elgallouchi <helgallouchi@gmail.com>
2022-04-02 00:09:34 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Landing from "./pages/Landing";
+import Landing from "./pages/Country";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllCountries from "./pages/AllCountries";
@@ -7,9 +7,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:name" element={<Landing />}></Route>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/:name" element={<Landing />}></Route>
         <Route path="/countries" element={<AllCountries />}></Route>
+        <Route path="*" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,12 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <Link to="/home">
+        <Link to="/">
           <BiWorld />
           <h2>Wolrd Countries</h2>
         </Link>
         <div className="menu">
           <Link to="/">accueil</Link>
-          <Link to="/home">home</Link>
           <Link to="/countries">countries</Link>
         </div>
       </div>
