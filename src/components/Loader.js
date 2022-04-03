31384d88fa4312ebb65d68168e5719106691b342
elgallouchi <helgallouchi@gmail.com>
2022-04-03 23:55:31 +0200

    add loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function loader() {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
